507c9f207f5daa96ade41515e98e5f049cef52f4
Tom Chauvel <tom.chauvel@proton.me>
2024-06-27 18:20:00 +0200

    download,modif

--- app/users/[slug]/page.tsx
+++ app/users/[slug]/page.tsx
@@ -1,76 +1,83 @@
 "use client"
-import { Table, TableBody, TableCell, TableHead, TableRow } from "@mui/material"
-import { useEffect, useState } from "react"
+import {Button, Table, TableBody, TableCell, TableRow} from "@mui/material"
+import {useEffect, useState} from "react"
 import QRCode from "react-qr-code"
+import {send} from "@/app/globals";
 
 type UserType = {
-  ID:string,
-  name:string,
-  lastname:string,
-  softs:number,
-  bieres:number,
-  forts:number
+    ID: string,
+    name: string,
+    lastname: string,
+    softs: number,
+    bieres: number,
+    forts: number
 }
 
-export default function Page({ params }: { params: { slug: string } }) {
-  
-  let [data,setData] = useState<UserType>({ID:"",name:"",lastname:"",softs:0,bieres:0,forts:0})
+export default function Page({params}: { params: { slug: string } }) {
 
-  useEffect(() =>{
-    fetch("https://rnwlvwlnab.execute-api.eu-west-3.amazonaws.com/Prod/users").then((resp) => {
-      return resp.json()
-    }).then((resp)=>{
-      console.log(resp)
-      for (let val of resp.data){
-        if (val.ID === params.slug){
-          setData(val)
-          break
-        }
-      }
-    })
-  }, [])
+    let [data, setData] = useState<UserType>({ID: "", name: "", lastname: "", softs: 0, bieres: 0, forts: 0})
 
-  return (
-    <main>
-      <QRCode value={data.ID}/>
+    useEffect(() => {
+        fetch("https://rnwlvwlnab.execute-api.eu-west-3.amazonaws.com/Prod/users").then((resp) => {
+            return resp.json()
+        }).then((resp) => {
+            console.log(resp)
+            for (let val of resp.data) {
+                if (val.ID === params.slug) {
+                    setData(val)
+                    break
+                }
+            }
+        })
+    }, [])
 
-      <Table>
-        <TableBody>
-        <TableRow key="ID">
-              <TableCell>ID</TableCell>
-              <TableCell>{data.ID}</TableCell>
-              <TableCell></TableCell>
-            </TableRow>
-            <TableRow key="name">
-              <TableCell>Prénom</TableCell>
-              <TableCell>{data.name}</TableCell>
-              <TableCell></TableCell>
-            </TableRow>
-            <TableRow key="lastname">
-              <TableCell>Nom</TableCell>
-              <TableCell>{data.lastname}</TableCell>
-              <TableCell></TableCell>
-            </TableRow>
-            <TableRow key="softs">
-              <TableCell>Softs</TableCell>
-              <TableCell>{data.softs}</TableCell>
-              <TableCell></TableCell>
-            </TableRow>
-            <TableRow key="bieres">
-              <TableCell>Bières</TableCell>
-              <TableCell>{data.bieres}</TableCell>
-              <TableCell></TableCell>
-            </TableRow>
-            <TableRow key="forts">
-              <TableCell>Forts</TableCell>
-              <TableCell>{data.forts}</TableCell>
-              <TableCell></TableCell>
-            </TableRow>
+    return (
+        <main>
+            <QRCode value={data.ID}/>
 
-          </TableBody>
-    </Table>
+            <Table>
+                <TableBody>
+                    <TableRow key="ID">
+                        <TableCell>ID</TableCell>
+                        <TableCell>{data.ID}</TableCell>
+                        <TableCell></TableCell>
+                    </TableRow>
+                    <TableRow key="name">
+                        <TableCell>Prénom</TableCell>
+                        <TableCell>{data.name}</TableCell>
+                        <TableCell></TableCell>
+                    </TableRow>
+                    <TableRow key="lastname">
+                        <TableCell>Nom</TableCell>
+                        <TableCell>{data.lastname}</TableCell>
+                        <TableCell></TableCell>
+                    </TableRow>
+                    <TableRow key="softs">
+                        <TableCell>Softs</TableCell>
+                        <TableCell>{data.softs}</TableCell>
+                        <TableCell><Button onClick={() => {
+                            send({ID: data.ID, softs: data.softs + 1}, "PATCH")
+                        }}>Ajouter</Button></TableCell>
+                    </TableRow>
+                    <TableRow key="bieres">
+                        <TableCell>Bières</TableCell>
+                        <TableCell>{data.bieres}</TableCell>
+                        <TableCell><Button onClick={() => {
+                            send({ID: data.ID, bieres: data.bieres + 1}, "PATCH")
+                        }}>Ajouter</Button></TableCell>
+                    </TableRow>
+                    <TableRow key="forts">
+                        <TableCell>Forts</TableCell>
+                        <TableCell>{data.forts}</TableCell>
+                        <TableCell><Button onClick={() => {
+                            send({ID: data.ID, forts: data.forts + 1}, "PATCH")
+                        }}>Ajouter</Button></TableCell>
+                    </TableRow>
+
+                </TableBody>
+            </Table>
 
 
-    </main>
-  );
+        </main>
+    );
 }
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,160 +1,185 @@
 "use client"
-import { Add, More, MoreVert } from "@mui/icons-material";
-import { Box, Button, Fab, Link, Menu, MenuItem, Modal, Paper, Table, TableBody, TableCell, TableHead, TableRow, TextField } from "@mui/material";
-import { useEffect, useState } from "react";
-import QRCode from "react-qr-code";
+import {Add, Clear, Download, MoreVert} from "@mui/icons-material";
+import {
+  Box,
+  Button,
+  Link,
+  Menu,
+  MenuItem,
+  Modal,
+  SpeedDial,
+  SpeedDialAction,
+  SpeedDialIcon,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  TextField
+} from "@mui/material";
+import {useEffect, useState} from "react";
+import {send} from "@/app/globals";
 
 type UserType = {
-  ID:string,
-  name:string,
-  lastname:string,
-  softs:number,
-  bieres:number,
-  forts:number
+    ID: string,
+    name: string,
+    lastname: string,
+    softs: number,
+    bieres: number,
+    forts: number
 }
-
 
 
 export default function Home() {
 
-  let [data,setData] = useState({data:[]})
-  const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null);
+    let [data, setData] = useState<{ data: UserType[] }>({data: []})
+    const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null);
 
-  useEffect(() =>{
-    fetch("https://rnwlvwlnab.execute-api.eu-west-3.amazonaws.com/Prod/users").then((resp) => {
-      return resp.json()
-    }).then((resp)=>{
-      setData(resp)
-    })
-  }, [])
-  
-  async function send(body:Object,method:string): Promise<Response> {      
+    useEffect(() => {
+        fetch("https://rnwlvwlnab.execute-api.eu-west-3.amazonaws.com/Prod/users").then((resp) => {
+            return resp.json()
+        }).then((resp) => {
+            setData(resp)
+        })
+    }, [])
 
-    let req = await fetch("https://rnwlvwlnab.execute-api.eu-west-3.amazonaws.com/Prod/users/update", {
-        method: method,
-        body: JSON.stringify(body),
-    })
-    
-    let resp = await req.json()
+    const [open, setOpen] = useState(false);
+    const [menuopen, setMenuOpen] = useState(false);
+    const handleOpen = () => {
+        setOpen(true);
+    };
+    const handleClose = () => {
+        setOpen(false);
+    };
+    const handleSave = async () => {
+        if (name != "" && lastname != "") {
+            await send({name: name, lastname: lastname, softs: 0, bieres: 0, forts: 0}, "PUT")
+            handleClose();
+        }
+    };
 
-    alert(resp.message)
-    location.reload();
-    return req
-}
+    const handleReset = async () => {
+        for (let item of data.data) {
+            await send({ID: item.ID, softs: 0, bieres: 0, forts: 0}, "PATCH", false)
+        }
+        location.reload();
+    };
 
-  const [open, setOpen] = useState(false);
-  const [menuopen, setMenuOpen] = useState(false);
-  const handleOpen = () => {
-    setOpen(true);
-  };
-  const handleClose = () => {
-    setOpen(false);
-  };
-  const handleSave = async() => {
-    if (name != "" && lastname != ""){
-      await send({name:name,lastname:lastname,softs:0,bieres:0,forts:0},"PUT")
-      handleClose();
+    const handleDownload = () => {
+
+        let text = "ID,name,lastname,softs,bieres,forts\n"
+        for (let item of data.data) {
+            text += `${item.ID},${item.name},${item.lastname},${item.softs},${item.bieres},${item.forts}\n`;
+        }
+
+        var element = document.createElement("a");
+        element.setAttribute(
+            "href",
+            "data:csv/plain;charset=utf-8," + encodeURIComponent(text)
+        );
+        element.setAttribute("download", "export.csv");
+        element.style.display = "none";
+        document.body.appendChild(element);
+        element.click();
+        document.body.removeChild(element);
     }
-  };
 
-  let [name,setName] = useState<string>("")
-  let [lastname,setLastName] = useState<string>("")
-  let [selected,setSelected] = useState<string>("")
+    let [name, setName] = useState<string>("")
+    let [lastname, setLastName] = useState<string>("")
+    let [selected, setSelected] = useState<string>("")
 
-  return (
-    <div>
+    return (
+        <div>
 
-    <Fab onClick={handleOpen} className="fixed left-6 bottom-20" color="primary" aria-label="add">
-      <Add />
-    </Fab>
+            <SpeedDial className="fixed left-6 bottom-20" ariaLabel="menu" icon={<SpeedDialIcon/>}>
+                <SpeedDialAction
+                    onClick={handleOpen}
+                    icon={<Add/>}
+                />
+                <SpeedDialAction
+                    onClick={handleReset}
+                    icon={<Clear/>}
+                />
+                <SpeedDialAction
+                    onClick={handleDownload}
+                    icon={<Download/>}
+                />
+            </SpeedDial>
+            <Modal open={open} onClose={handleClose} aria-labelledby="parent-modal-title"
+                   aria-describedby="parent-modal-description" className="grid place-items-center h-full">
+                <Box sx={{}} className="w-96 bg-slate-50 p-4 flex flex-col shadow border gap-2">
+                    <h2 className="text-xl font-bold">Ajouter une personne</h2>
+                    <TextField error={lastname == ""} value={lastname} onChange={e => setLastName(e.target.value)}
+                               id="nom" label="Nom" variant="filled"/>
+                    <TextField error={name == ""} value={name} onChange={e => setName(e.target.value)} id="prénom"
+                               label="Prénom" variant="filled"/>
+                    <div className="flex w-full">
+                        <Button className="flex-1" onClick={handleSave}>Add</Button>
+                        <Button className="flex-1" onClick={handleClose}>Cancel</Button>
+                    </div>
+                </Box>
+            </Modal>
+
+            <Table>
+                <TableHead>
+                    <TableRow>
+                        <TableCell>ID</TableCell>
+                        <TableCell>Nom</TableCell>
+                        <TableCell>Prénom</TableCell>
+                        <TableCell>Softs</TableCell>
+                        <TableCell>Bières</TableCell>
+                        <TableCell>Forts</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {data.data.map((user: UserType) =>
+                        <TableRow key={user.ID}>
+                            <TableCell>{user.ID}</TableCell>
+                            <TableCell>{user.lastname}</TableCell>
+                            <TableCell>{user.name}</TableCell>
+                            <TableCell>{user.softs}</TableCell>
+                            <TableCell>{user.bieres}</TableCell>
+                            <TableCell>{user.forts}</TableCell>
 
 
+                            <TableCell>
+                                <Button
+                                    id="basic-button"
+                                    aria-controls={open ? 'basic-menu' : undefined}
+                                    aria-haspopup="true"
+                                    aria-expanded={open ? 'true' : undefined}
+                                    onClick={e => {
+                                        setAnchorEl(e.currentTarget);
+                                        setSelected(user.ID);
+                                        setMenuOpen(true);
+                                    }}
+                                >
+                                    <MoreVert></MoreVert>
+                                </Button>
+                            </TableCell>
 
 
+                        </TableRow>
+                    )}
+                </TableBody>
+            </Table>
+            <Menu
+                id="basic-menu"
+                open={menuopen}
+                onClose={() => setMenuOpen(false)}
+                anchorEl={anchorEl}
+                MenuListProps={{
+                    'aria-labelledby': 'basic-button',
+                }}
+            >
+                <Link href={`/users/${selected}`}><MenuItem>Show Profile</MenuItem></Link>
+                <MenuItem onClick={() => {
+                }}>Show QRCODE</MenuItem>
+                <MenuItem onClick={() => send({ID: selected}, "DELETE")}>Delete</MenuItem>
+            </Menu>
 
-
-
-
-
-
-
-
-
-
-
-
-
-    <Modal open={open} onClose={handleClose} aria-labelledby="parent-modal-title" aria-describedby="parent-modal-description" className="grid place-items-center h-full">
-      <Box sx={{}} className="w-96 bg-slate-50 p-4 flex flex-col shadow border gap-2">
-        <h2 className="text-xl font-bold">Ajouter une personne</h2>
-        <TextField error={lastname==""} value={lastname} onChange={e => setLastName(e.target.value)} id="nom" label="Nom" variant="filled" />
-        <TextField error={name==""} value={name} onChange={e => setName(e.target.value)}  id="prénom" label="Prénom" variant="filled" />
-        <div className="flex w-full">
-          <Button  className="flex-1" onClick={handleSave}>Add</Button>
-          <Button className="flex-1" onClick={handleClose}>Cancel</Button>
         </div>
-      </Box>
-    </Modal>
-
-    <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell>ID</TableCell>
-            <TableCell>Nom</TableCell>
-            <TableCell>Prénom</TableCell>
-            <TableCell>Softs</TableCell>
-            <TableCell>Bières</TableCell>
-            <TableCell>Forts</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {data.data.map((user:UserType) =>
-            <TableRow key={user.ID}>
-              <TableCell>{user.ID}</TableCell>
-              <TableCell>{user.lastname}</TableCell>
-              <TableCell>{user.name}</TableCell>
-              <TableCell>{user.softs}</TableCell>
-              <TableCell>{user.bieres}</TableCell>
-              <TableCell>{user.forts}</TableCell>
-
-
-
-              <TableCell>
-              <Button
-        id="basic-button"
-        aria-controls={open ? 'basic-menu' : undefined}
-        aria-haspopup="true"
-        aria-expanded={open ? 'true' : undefined}
-        onClick={e => {setAnchorEl(e.currentTarget);setSelected(user.ID);setMenuOpen(true);    }}
-    >
-        <MoreVert></MoreVert>
-    </Button>
-                  </TableCell>
-
-
-
-
-
-            </TableRow>
-          )}
-        </TableBody>
-    </Table>
-    <Menu
-        id="basic-menu"
-        open={menuopen}
-        onClose={() => setMenuOpen(false)}
-        anchorEl={anchorEl}
-        MenuListProps={{
-        'aria-labelledby': 'basic-button',
-        }}
-    >
-      <Link href={`/users/${selected}`}><MenuItem>Show Profile</MenuItem></Link>
-      <MenuItem onClick={() => {}}>Show QRCODE</MenuItem>
-      <MenuItem onClick={() => send({ID:selected},"DELETE")}>Delete</MenuItem>
-    </Menu>
-
-    </div>
-  );
+    );
 
     //<TableCell><QRCode value={user.ID}/></TableCell>
 
